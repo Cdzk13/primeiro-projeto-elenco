e0360c8bdad778a167726c6bb6bdb838fd4c9001
Cdzk13 <devcardoso13@gmail.com>
2024-09-09 19:20:33 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,30 @@
+let dados = [
+    {
+        titulo: "Fernandinho",
+        descricao: "Fernando Luiz Roza, é um experiente volante brasileiro que atualmente defende as cores do Athletico Paranaense. Com uma longa e vitoriosa carreira, o atleta já passou por grandes clubes como Shakhtar Donetsk e Manchester City, onde conquistou diversos títulos.",
+        link: "https://en.wikipedia.org/wiki/Fernandinho_(footballer,_born_May_1985)",
+        tags: "craque ídolo capitão"
+    },
+    {
+        titulo: "Agustin Cannobio",
+        descricao: "Agustin Cannobio é um futebolista uruguaio que atua como ponta-direita.. Conhecido por sua habilidade e velocidade, Cannobio é uma das promessas da equipe paranaense.",
+        link: "https://pt.wikipedia.org/wiki/Agustín_Canobbio" ,
+        tags: "velocista motorzinho"
+    },  
+    {
+        titulo: "Gonzalo Mastriani",
+        descricao: "Gonzalo Mastriani é um atacante uruguaio que atualmente defende as cores do Athletico Paranaense. Com sua habilidade de finalização e visão de jogo, Mastriani se tornou um dos principais artilheiros da equipe.",
+        link: "https://pt.wikipedia.org/wiki/Gonzalo_Mastriani",
+        tags: "matador artilheiro goleador"
+    },
+    {
+        titulo: "Esquivel",
+        descricao: "Zagueiro argentino conhecido por sua forte marcação e liderança em campo. Chegou ao Athletico Paranaense em [data da transferência] e rapidamente se tornou peça fundamental na defesa da equipe.",
+        link: "https://www.transfermarkt.com.br/lucas-esquivel/profil/spieler/829969"
+      },
+      {
+        titulo: "Pablo",
+        descricao: "Atacante brasileiro com grande velocidade e habilidade nos dribles. Destacou-se em outras equipes antes de chegar ao Athletico Paranaense e é um dos principais artilheiros da equipe.",
+        link: "https://pt.wikipedia.org/wiki/Pablo_Felipe_Teixeira"
+      }
+];
